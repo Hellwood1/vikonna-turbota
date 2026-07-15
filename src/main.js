06613695967408a52cc51abca54ba018beffc547
Hellwood1 <samburik123@gmail.com>
2026-07-15 22:13:08 +0200

    Upd. v. 0.1.
    - Створив базу проекту на збірнику "Vanila";
    - Створив структуру відповідно до макету;
    - Завантажив та підключив шрифти;
    - Налаштував залежності;

---
+++ src/main.js
@@ -0,0 +1,7 @@
+'use strict';
+import './js/mobile-menu.js';
+import './js/language-switcher.js';
+import './js/services-tabs.js';
+import './js/partners-marquee.js';
+import './js/faq-accordion.js';
+import './js/contact-form.js';
